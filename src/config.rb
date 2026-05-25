a13b4f3fad1386c9f9b4b5965d68080ed2479292
Masaki Haruka <yek@reasonset.net>
2026-05-26 02:33:52 +0900

    Default is default

--- src/config.rb
+++ src/config.rb
@@ -2,7 +2,7 @@ $config = {
   use_metadata: !["false", "no", "disable"].include?(ENV["USE_METADATA"]),
   use_thumbnail: !["false", "no", "disable"].include?(ENV["USE_THUMBNAIL"]),
   ffprobe_cmd: "ffprobe",
-  videoplayer: "vidstack", # vidstack, default
+  videoplayer: "default", # vidstack, default
 
   # Generally, these settings should not be changed.
   media_root: ENV["MEDIA_ROOT"].sub(%r:/$:, ""),
